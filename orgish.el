3f91b9db5cedaf68226777252c62e8e2c068ddfc
ChanderG <mail@chandergovind.org>
2023-05-09 15:15:42 +0530

    ensure writes of file happen cleanly
    saved buffers don't show up in the save-some-buffers function
    this is needed to make the lifecycle a bit more smooth

--- orgish.el
+++ orgish.el
@@ -6,6 +6,10 @@
     (insert "\n")
     (insert "org -->\n")
     (write-region nil nil file))
+  ; notify to emacs that the file has no more changes
+  ; this is needed to ensure that this buffer doesn't come up in save-some-buffers group
+  (with-current-buffer buffer
+    (set-buffer-modified-p nil))
   t
   )
 
@@ -15,8 +19,11 @@
     ()
     ; doesn't exist - create a new buffer
     (progn
-      (find-file-literally (concat filename "-orgish"))
+      (find-file-literally (concat "*" filename "-orgish" "*"))
       (org-mode)
       (setq org-export-show-temporary-export-buffer nil)
+      ; setup the filename of the buffer to point to the source html file
+      ; can be nil, but you can accidently close the buffer without saving
+      (setq buffer-file-name filename)
       (add-hook 'write-contents-functions (apply-partially 'orgish/save-buffer (buffer-name) filename))
       )))
