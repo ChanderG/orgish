8b4b9fb4037b61f1b4204f822ee20b657a4cc97e
ChanderG <mail@chandergovind.org>
2023-05-09 17:09:01 +0530

    open existing files correctly, other helpers
    + open existing files: by reading out the relevant portions from the comments
    + add focus out hook to make easier saving
    
    WARNING: can't use evil's :w, since that calls evil-write and not
    save-buffer. Use save-buffer directly, or bind it somewhere easier
    than C-x C-s

--- orgish.el
+++ orgish.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding:t -*-
+
 (defun orgish/save-buffer (buffer file)
   (with-current-buffer (org-html-export-as-html)
     ; insert our source org stuff in
@@ -13,17 +15,37 @@
   t
   )
 
+(defun orgish/extract-source (file buffer)
+  "Extract out the source org from the html file."
+  (with-temp-buffer
+    (insert-file-contents-literally file)
+    (search-forward "<!-- org\n")
+    (let* ((beg (point))
+           (end (progn
+                  (search-forward "org -->\n")
+                  (forward-line -1)
+                  (point)))
+           (sbuf (current-buffer)))
+        (with-current-buffer buffer
+          (insert-buffer-substring sbuf beg end)
+          )))
+  t)
+
 (defun orgish/open (filename)
   (interactive)
+  ; open this file - which shouldn't exist in the first place
+  ; so a new fresh buffer should be created
+  (find-file-literally (concat "*" filename "-orgish" "*"))
+  ; check if we are opening an existing orgish file
   (if (file-exists-p filename)
-    ()
-    ; doesn't exist - create a new buffer
-    (progn
-      (find-file-literally (concat "*" filename "-orgish" "*"))
-      (org-mode)
-      (setq org-export-show-temporary-export-buffer nil)
-      ; setup the filename of the buffer to point to the source html file
-      ; can be nil, but you can accidently close the buffer without saving
-      (setq buffer-file-name filename)
-      (add-hook 'write-contents-functions (apply-partially 'orgish/save-buffer (buffer-name) filename))
-      )))
+    (if (not (ignore-errors (orgish/extract-source filename (current-buffer))))
+        ; some errors in parsing
+        (error "Source file does not seem to be orgish. Refusing to continue!")))
+  ; do the normal processing here
+  (org-mode)
+  (setq org-export-show-temporary-export-buffer nil)
+  ; unset the file connection - since this is not a normal buffer
+  (setq buffer-file-name nil)
+  ; autosave on focus out
+  (add-hook 'focus-out-hook 'save-buffer 0 t)
+  (add-hook 'write-contents-functions (apply-partially 'orgish/save-buffer (buffer-name) filename)))
